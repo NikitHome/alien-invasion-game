5a09720f0a59c269a5da0290369f40a0de0a9d45
Nikita Khomenko <mishanikita261202@mail.ru>
2024-09-16 17:46:28 +0300

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings():
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        
+        self.bg_color = (210, 210, 210)
+        
+        self.ship_speed = 2
+        
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        
+        self.alien_speed = 1.0
