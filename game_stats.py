b9057e76f1b130e595f15162f7affe26051a0a5b
Nikita Khomenko <mishanikita261202@mail.ru>
2024-10-24 16:46:36 +0300

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -1,12 +1,19 @@
 class GameStats():
+    """Отслеживание статистики для игры Alien Invasion."""
+    
     def __init__(self, settings):
+        """Инициализирует статистику."""
         self.settings = settings
         self.reset_stats()
+        
+        # Игра Alien Invasion запускается в неактивном состоянии.
         self.game_active = False
         
+        # Рекорд не должен сбрасываться.
         self.high_score = 0
         
     def reset_stats(self):
+        """Инициализирует статистику, изменяющуюся в ходе игры."""
         self.ship_left = self.settings.ship_limit
         self.score = 0
         self.level = 1
